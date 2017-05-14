8cc57c3d396858848d0d1adbbfd9bedc455cbad8
stefan-hering <stefanvhering@gmail.com>
2017-05-14 22:45:29 +0200

    Removed while loop

--- src/main/scala/sh/webserver/Server.scala
+++ src/main/scala/sh/webserver/Server.scala
@@ -1,16 +1,18 @@
 package sh.webserver
 
 import java.net.ServerSocket
-import java.util.concurrent.Executors
+import java.util.concurrent.{ExecutorService, Executors}
 
-class Server(port: Int) extends Runnable {
-  def run() {
+class Server(port: Int) {
+  def start() {
     val server = new ServerSocket(port);
     val pool = Executors.newFixedThreadPool(8);
-    while(true){
-      val socket = server.accept();
-      pool.execute(new RequestHandler(socket));
-      
-    }
+    listen(server, pool);
+  }
+
+  def listen(server : ServerSocket,pool : ExecutorService) {
+    val socket = server.accept();
+    pool.execute(new RequestHandler(socket));
+    listen(server, pool);
   }
 }
